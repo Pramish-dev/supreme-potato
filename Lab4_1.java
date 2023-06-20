2e70051d4df2a2dd5413ebb49f64a17176134119
Pramish <119416585+Pramish-dev@users.noreply.github.com>
2023-06-20 21:52:05 +0545

    java class and obj

---
+++ Lab4_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab4_1{
+    int num = 23;
+    String name;
+
+    public static void main(String[] args){
+        Lab4_1 count =new Lab4_1();
+        System.out.println(count.num);
+        System.out.println(count.name="Pramish");
+    }
+}
