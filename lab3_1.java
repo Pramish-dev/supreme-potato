e48b93650e415a61b3f1b3c5fe42f1c77c468067
Pramish <119416585+Pramish-dev@users.noreply.github.com>
2023-06-20 21:51:36 +0545

    java code

---
+++ lab3_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class lab3_1 {
+  public static void main(String[] args) {
+    Scanner scanner =new Scanner(System.in);
+    int num;
+    System.out.println("enter any number: ");
+    System.out.println("\n\n");
+    System.out.println("Enter an integer: ");
+     num = scanner.nextInt();
+     if(num>0){
+        System.out.println("Number is positive");
+     }
+  }
+}
+
