d5a5f40cb5f445622a25e449ec119e2c36cb74f0
Pramish <119416585+Pramish-dev@users.noreply.github.com>
2023-06-20 21:52:33 +0545

    oop in java

---
+++ lab4_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class lab4_2{
+    lab4_2(){
+       
+        System.out.println('constructor is created');
+            
+        }
+    
+        public static void main(String[] args){
+            lab4_2 obj = new lab4_2();
+            
+    
+}}
